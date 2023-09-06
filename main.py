9607cc7e09acc4b948c15d36e5e41aaaa6205834
Ethan Rockwood (US) <rockwood@battelleecology.org>
2023-09-06 10:04:21 -0600

    Add Qodana and act on code analysis results

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def get_args(message):
 async def check_success(interaction, guess, adjustment, result, leverage):
     """
     This function compares the discord user's guess and their adjustment range to the
-    radomly generated number that corresponds to the specified star/difficulty level.
+    randomly generated number that corresponds to the specified star/difficulty level.
     Outputs to the discord channel with results.
 
     :param interaction: The discord bot interaction received by the command
@@ -59,15 +59,20 @@ async def check_success(interaction, guess, adjustment, result, leverage):
     check_set = success_range.intersection(guess_range)
     if guess == result:
         await interaction.response.send_message(
-            f'Your crafting check was a critical success! Your guess was {guess}, which matched the generated Trial value of {result} exactly!'
+            f'Your crafting check was a critical success! Your guess was {guess}, which matched the generated Trial '
+            f'value of {result} exactly!'
         )
     elif len(check_set) != 0:
         await interaction.response.send_message(
-            f'Your crafting check succeeded. Your guess was {guess}. It covered an effective range of: {min(guess_range)}-{max(guess_range)}.\n\nThe random number was {result}. The range your effective number needed to fall inside of was: {min(success_range)} - {max(success_range)}'
+            f'Your crafting check succeeded. Your guess was {guess}. It covered an effective range of: '
+            f'{min(guess_range)}-{max(guess_range)}.\n\nThe random number was {result}. The range your effective number'
+            f' needed to fall inside of was: {min(success_range)} - {max(success_range)}'
         )
     else:
         await interaction.response.send_message(
-            f'Your crafting check failed. Your guess was {guess}. It covered an effective range of: {min(guess_range)}-{max(guess_range)}.\n\nThe random number was {result}. The range your effective number needed to fall inside of was: {min(success_range)} - {max(success_range)}'
+            f'Your crafting check failed. Your guess was {guess}. It covered an effective range of: '
+            f'{min(guess_range)}-{max(guess_range)}.\n\nThe random number was {result}. The range your effective '
+            f'number needed to fall inside of was: {min(success_range)} - {max(success_range)}'
         )
     return 0
 
@@ -116,7 +121,9 @@ async def craft(interaction: discord.Interaction, trial: int, stars: int, adjust
         await check_success(interaction, guess, adjustment, result, leverage)
 
     except (Exception,):
-        await interaction.response.send_message(f'Failed to process your message!\nPlease ensure you only used one space between each argument, there are no typos, and you provided arguments in the format argument:value.')
+        await interaction.response.send_message(
+            f'Failed to process your message!\nPlease ensure you only used one space between each argument, there are '
+            f'no typos, and you provided arguments in the format argument:value.')
     finally:
         print("Command for Crafting was Successfully Processed\n")
         return 0
@@ -134,7 +141,9 @@ async def random(interaction: discord.Interaction, low: int, high: int):
 
         await interaction.response.send_message(f'Random Number is: {result}')
     except (Exception,):
-        await interaction.response.send_message(f'Encountered an issue with the formatting of your request for a random number!\nPlease ensure there are no typos, you only provided min:number max:number, and that there are no excess spaces')
+        await interaction.response.send_message(
+            f'Encountered an issue with the formatting of your request for a random number!\nPlease ensure there are '
+            f'no typos, you only provided min:number max:number, and that there are no excess spaces')
     finally:
         print("Command for Random was Successfully Processed\n")
         return 0
@@ -147,15 +156,29 @@ async def random(interaction: discord.Interaction, low: int, high: int):
 async def larphelp(interaction: discord.Interaction, help_term: str):
     try:
         if help_term == "craft":
-            message = f'To craft, use: ```/craft trial:check_difficulty_as_a_number stars:star_count adjustment:adjustment_number guess:your_guess```\n\tExample: ```/craft trial:3 stars:3 adjustment:13 guess:14```\n'
-            message += f'Use the following stat as your adjustment number for the corresponding crafting type:\n\tApothecary (Alchemy): Wits\n\tArcane Scribe (Scribing): Magic\n\tArmorer (Armor Smithing): Willpower\n\tEnchanter (Imbuing): Magic\n\tTinkerer (Tinkering): Luck\n\tWeapon Smith (Weapon Smithing): Might\n\nTrial Check should be provided as a number 0-5, mapped from trivial to impossible.\n\tTrivial: 0\n\tEasy: 1\n\tMedium: 2\n\tModerate: 3\n\tHard: 4\n\tImpossible: 5'
+            message = (f'To craft, use: ```/craft trial:check_difficulty_as_a_number stars:star_count '
+                       f'adjustment:adjustment_number guess:your_guess```\n\tExample: ```/craft trial:3 stars:3 '
+                       f'adjustment:13 guess:14```\n')
+            message += (f'Use the following stat as your adjustment number for the corresponding crafting '
+                        f'type:\n\tApothecary (Alchemy): Wits\n\tArcane Scribe (Scribing): Magic\n\tArmorer (Armor '
+                        f'Smithing): Willpower\n\tEnchanter (Imbuing): Magic\n\tTinkerer (Tinkering): Luck\n\tWeapon '
+                        f'Smith (Weapon Smithing): Might\n\nTrial Check should be provided as a number 0-5, '
+                        f'mapped from trivial to impossible.\n\tTrivial: 0\n\tEasy: 1\n\tMedium: 2\n\tModerate: '
+                        f'3\n\tHard: 4\n\tImpossible: 5')
             await interaction.response.send_message(message)
         elif help_term == "random":
-            await interaction.response.send_message(f'To use the random function, provide your desired rage in the format:```/random min:minimum_number max:max_number```')
+            await interaction.response.send_message(f'To use the random function, provide your desired rage in the '
+                                                    f'format:```/random min:minimum_number max:max_number```')
         else:
-            await interaction.response.send_message(f'Help Command not recognized!\nLARPBot Usage information:\n\tSupported Functions are:\n\t\trandom\n\t\tcraft\n\nTo get help on a specific function, type ```/help function_name```')
+            await interaction.response.send_message(f'Help Command not recognized!\nLARPBot Usage '
+                                                    f'information:\n\tSupported Functions '
+                                                    f'are:\n\t\trandom\n\t\tcraft\n\nTo get help on a specific '
+                                                    f'function, type ```/help function_name```')
     except (Exception,):
-        await interaction.response.send_message(f'Experienced an issue processing your help request.\nLARPBot Usage information:\n\tSupported Functions are:\n\t\trandom\n\t\tcraft\n\nTo get help on a specific function, type ```/help function_name```')
+        await interaction.response.send_message(f'Experienced an issue processing your help request.\nLARPBot Usage '
+                                                f'information:\n\tSupported Functions '
+                                                f'are:\n\t\trandom\n\t\tcraft\n\nTo get help on a specific function, '
+                                                f'type ```/help function_name```')
     finally:
         print("Command for Help was Successfully Processed\n")
         return 0
